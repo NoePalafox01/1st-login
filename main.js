541acddfa3ca39942a1febb3cc12adb9c7c2a21b
Noe Palafox <palafoxmorales.noeabdiel@gmail.com>
2023-06-12 10:13:04 -0700

    login finalizado

---
+++ main.js
@@ -0,0 +1,13 @@
+//probar en consola
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+const button = document.getElementById('button')
+
+button.addEventListener('click', (e)=>{
+    e.preventDefault()
+    const datos = {
+        username: username.value,
+        password: password.value
+    }
+    console.log(datos)
+})
